b81353285df63135cad5eeedf160c6aa7c312e8e
Ashley-Lijin <ashleylijin7@gmail.com>
2023-02-05 13:13:56 +0530

    tailwind classname is hiddden

--- src/Components/hero.jsx
+++ src/Components/hero.jsx
@@ -3,11 +3,11 @@ import React from 'react'
 const Hero = () => {
   return (
     <div>
-        <div className='dark:bg-neutral-900 bg-slate-100 bg-fixed h-screen bg-no-repeat'>
+        {/* <div className='dark:bg-neutral-900 bg-slate-100 bg-fixed h-screen bg-no-repeat'>
             <div className='dark:text-white text-black pt-96'>
               <h1>THE BUGATTI CHIRON PROFILÉE <br /> AN AUTOMOTIVE SOLITAIRE</h1>
             </div>
-        </div>
+        </div> */}
     </div>
   )
 }
